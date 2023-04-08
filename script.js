c5c5c6179f75b184b73d43580817f7f8f7d826ad
VardhanKumar307 <115169773+VardhanKumar307@users.noreply.github.com>
2023-04-08 10:05:05 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,23 +1,26 @@
 //complete this code
 class Rectangle {
-	constructor(width, height) {
-    this.width = width;
-    this.height = height;
-  }
-
-  getArea() {
-    return this.width * this.height;
+	def __init__(self, width, height):
+        self._width = width
+        self._height = height
+    
+    def getWidth(self):
+        return self._width
+    
+    def getHeight(self):
+        return self._height
+    
+    def getArea(self):
+        return self._width * self._height
   }
 }
 
-class Square extends Animal {
-	constructor(side) {
-    super(side, side);
-  }
-
-  getPerimeter() {
-    return 4 * this.width;
-  }
+class Square(Rectangle) {
+	def __init__(self, side):
+        super().__init__(side, side)
+    
+    def getPerimeter(self):
+        return 4 * self._width
 }
 
 // Do not change the code below this line
